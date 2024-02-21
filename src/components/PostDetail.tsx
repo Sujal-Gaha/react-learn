838f5ab0aca1eb3dd07bbb6b58a0650816e39969
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-02-21 16:43:41 +0545

    added style to the post page

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -33,10 +33,86 @@ function PostData(props: { postId: number }) {
   }, []);
 
   return (
-    <div>
-      <p>Id: {post?.id}</p>
-      <p>Title: {post?.title}</p>
-      <p>Body: {post?.body}</p>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        flexDirection: "column",
+        border: "2px solid gray",
+        borderRadius: "4px",
+        backgroundColor: "lightgray",
+        margin: "60px auto",
+        width: "50%",
+        padding: "0 80px",
+      }}
+    >
+      <h2
+        style={{
+          fontWeight: "bold",
+          color: "lightpink",
+          padding: "10px 25px",
+        }}
+      >
+        Post no. {post?.id}
+      </h2>
+      <h1
+        style={{
+          fontWeight: "bolder",
+          padding: "25px",
+          color: "darkorchid",
+        }}
+      >
+        {post?.title}
+      </h1>
+      <p
+        style={{
+          textAlign: "center",
+          padding: "28px 0",
+        }}
+      >
+        {post?.body}
+      </p>
+      <p
+        style={{
+          textAlign: "end",
+          paddingRight: "25px",
+          color: "violet",
+        }}
+      >
+        <u>---Posted by userId: {post?.userId}</u>
+      </p>
+      <hr
+        style={{
+          margin: "20px 25px 25px",
+        }}
+      />
+      <label
+        htmlFor="comment"
+        style={{
+          margin: "20px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <textarea
+          name="comment"
+          id="comment"
+          cols={60}
+          rows={2}
+          placeholder="Add a comment..."
+          style={{
+            resize: "none",
+            boxSizing: "border-box",
+            width: "45%",
+          }}
+        ></textarea>
+        <button style={{ width: "80px", height: "22px", alignSelf: "end" }}>
+          Comment
+        </button>
+        <button style={{ width: "80px", height: "22px", alignSelf: "end" }}>
+          Cancel
+        </button>
+      </label>
     </div>
   );
 }
