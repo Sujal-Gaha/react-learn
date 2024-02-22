09d514d8363c918e10b0302ac2b5d299d0f67761
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-02-22 20:47:59 +0545

    fetched the list of todos

--- src/components/TodosList.tsx
+++ src/components/TodosList.tsx
@@ -1,4 +1,24 @@
+import { useEffect } from "react";
+
 export function TodosList() {
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        const jsonData = response.json();
+        return jsonData;
+      })
+      .then((jsonData) => {
+        console.log("JsonData ", jsonData);
+      })
+      .catch((error) => {
+        console.log("Error ", error);
+      });
+  }, []);
   return (
     <div>
       <p>This is the actual todo list</p>
