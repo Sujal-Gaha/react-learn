d9d292301cdfad063609f2355e5498e974fc1d97
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-02-22 20:53:37 +0545

    rendered the todos list

--- src/components/TodosList.tsx
+++ src/components/TodosList.tsx
@@ -1,6 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+type TTodoITem = {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+}
 
 export function TodosList() {
+  const [todos, setTodos] = useState<TTodoITem[]>([]);
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos", {
       method: "GET",
@@ -14,6 +22,7 @@ export function TodosList() {
       })
       .then((jsonData) => {
         console.log("JsonData ", jsonData);
+        setTodos(jsonData);
       })
       .catch((error) => {
         console.log("Error ", error);
@@ -21,7 +30,18 @@ export function TodosList() {
   }, []);
   return (
     <div>
-      <p>This is the actual todo list</p>
+      <h2>List of Todos</h2>
+
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <li key={index}>
+              id: {todo.id}, userId: {todo.userId}, title: {todo.title},
+              completed: {todo.completed}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
